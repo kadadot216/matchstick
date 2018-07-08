6a510f8ed24652e3f1ac5f23dd0b9f77e7aeb6b0
kadadot216 <yoctopolochoon54@gmail.com>
2018-07-08 18:26:15 +0200

    Made a very basic, greedy-type AI

--- include/prototypes.h
+++ include/prototypes.h
@@ -11,6 +11,7 @@
 #include "types.h"
 
 void	display_board(game_board_t *board);
+void	print_help_msg(char *pname);
 game_status_t	play_game(game_board_t *board);
 game_board_t	*init_board(int nb_lines, int max_sticks);
 game_board_t	*unset_board(game_board_t *board);
--- src/ai/strategy.c
+++ src/ai/strategy.c
@@ -7,7 +7,33 @@
 
 #include "types.h"
 
-void	ai_get_input_choice(p_choice_t *choice, game_board_t *board)
+int	ai_select_line(game_board_t *b)
 {
+	uint_t	b_line = 0;
+	uint_t	i = 0;
 
+	while (i < b->max_lines) {
+		if (b->remmatches_atl[b_line] < b->remmatches_atl[i]) {
+			b_line = i;
+		}
+		i++;
+	}
+	return (b_line);
+}
+
+int	ai_select_nbmatches(game_board_t *b, uint_t sel_line)
+{
+	if (b->remmatches_atl[sel_line] < b->match_limit) {
+		return ((b->remmatches_atl[sel_line] - b->match_limit));
+	} else if (b->remmatches_atl[sel_line] == 2) {
+		return (1);
+	} else {
+		return (b->match_limit);
+	}
+}
+
+void	ai_get_input_choice(p_choice_t *c, game_board_t *b)
+{
+	c->line = ai_select_line(b);
+	c->matches = ai_select_nbmatches(b, c->line);
 }
