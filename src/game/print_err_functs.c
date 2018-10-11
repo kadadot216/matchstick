c2f0744b26e88a3c10bbfdb33c087e001e00ee53
kadadot216 <yoctopolochoon54@gmail.com>
2018-10-11 16:24:07 +0400

    Wrong input messages are in STDOUT

--- src/game/print_err_functs.c
+++ src/game/print_err_functs.c
@@ -9,27 +9,27 @@
 
 void	print_error_line_oor(void)
 {
-	my_putstr_fd(2, "Error: this line is out of range\n");
+	my_putstr_fd(1, "Error: this line is out of range\n");
 }
 
 void	print_error_invalid_prompt(void)
 {
-	my_putstr_fd(2, "Error: invalid input (positive number expected)\n");
+	my_putstr_fd(1, "Error: invalid input (positive number expected)\n");
 }
 
 void	print_error_matches_oob(int upper_bound)
 {
-	my_putstr_fd(2, "Error: you cannot remove more than ");
-	my_put_nbr_fd(2, upper_bound);
-	my_putstr_fd(2, " matches per turn\n");
+	my_putstr_fd(1, "Error: you cannot remove more than ");
+	my_put_nbr_fd(1, upper_bound);
+	my_putstr_fd(1, " matches per turn\n");
 }
 
 void	print_error_no_matches(void)
 {
-	my_putstr_fd(2, "Error: you have to remove at least one match\n");
+	my_putstr_fd(1, "Error: you have to remove at least one match\n");
 }
 
 void	print_error_not_enough_matches(void)
 {
-	my_putstr_fd(2, "Error: not enough matches on this line\n");
+	my_putstr_fd(1, "Error: not enough matches on this line\n");
 }
